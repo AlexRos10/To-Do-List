82c19eda441be095636f8afdba67117fd2353cba
AlexRos <63934839+AlexRos10@users.noreply.github.com>
2020-10-25 13:35:21 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,40 @@
+from airtable import *
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+base_key = 'appf1xLbya3v7if5n'
+table_name = 'Tasks'
+airtable = Airtable(base_key, table_name, 'keyXvtazELAUx8WtK')
+
+def get_tasks():
+    tasks = []
+    for page in airtable.get_iter():
+        for record in page:
+            value = record['fields']['Notes']
+            tasks.append(value)
+            
+    return tasks
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html', tasks=get_tasks())
+
+@app.route('/index', methods = ['GET', 'POST'])
+def index():
+    task = request.form['task']
+    airtable.insert({'Notes': task})
+
+    return redirect('/home')
+
+@app.route('/subtrac', methods = ['GET', 'POST'])
+def subtrac():
+    close = request.form['close']
+    record = airtable.match('Notes', close)
+    airtable.delete(record['id'])
+
+    return redirect("/home")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2000)
